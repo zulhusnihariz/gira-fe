05b6eb935fe12371a5fc47601da054070af41512
zulhusni hariz <zulhusnihariz97@gmail.com>
2021-11-21 11:45:18 +0800

    add custom theme color

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './app/styles/index.css'
 import MainLayout from './app/index.jsx'
+import { MuiThemeProvider, createTheme } from '@material-ui/core/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#282C34',
+    },
+    secondary: {
+      main: '#f0ebd8',
+    },
+    text: {
+      primary: '#f0ebd8',
+      secondary: '#f0ebd8',
+    },
+  },
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <MainLayout />
+    <MuiThemeProvider theme={theme}>
+      <MainLayout />
+    </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 )
